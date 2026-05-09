44fb5924b0ad41b228bd58810830719e187eded9
Isha-prog <rekhas.singh2004@gmail.com>
2026-05-09 21:31:48 +0530

    Converting integer to bits and toggle the bits to find the integer

---
+++ toggleBits.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class toggleBits {
+    
+
+  public static void main(String[] args)
+  {
+         Scanner sc=new Scanner(System.in);
+         int no=sc.nextInt();
+          String bin="";
+         
+          while(no!=0)
+           {
+                  bin=(no&1)+bin;
+                  no=no>>1;
+           }
+            bin=bin.replaceAll("1","2");
+            bin=bin.replaceAll("0","1");
+            bin=bin.replaceAll("2","0");
+            int res=Integer.parseInt(bin,2);
+           System.out.println(res);
+   }
+}
+
